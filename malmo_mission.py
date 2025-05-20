6036f5eb0469c68e1ea71c3c24c352c491e2edc7
Daphne <daphne922003@gmail.com>
2025-05-19 21:33:48 -0700

    update environment + add ghast

--- malmo_mission.py
+++ malmo_mission.py
@@ -27,72 +27,77 @@ missionXML='''<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
                   
                   <DrawingDecorator>
                     <!-- clearing mission envionrment to start on blank since missions build on top of each other apparently -->
-                    <DrawCuboid x1="0" y1="2" z1="0" x2="20" y2="20" z2="40" type="air"/>
+                    <DrawCuboid x1="0" y1="2" z1="0" x2="20" y2="40" z2="100" type="air"/>
                   
                     <!-- right wall -->
-                    <DrawCuboid x1="0" y1="0" z1="0" x2="0" y2="10" z2="30" type="stone"/>
+                    <DrawCuboid x1="0" y1="0" z1="0" x2="0" y2="20" z2="60" type="stone"/>
                     
                     <!-- left wall -->
-                    <DrawCuboid x1="15" y1="0" z1="0" x2="15" y2="10" z2="30" type="stone"/>
+                    <DrawCuboid x1="15" y1="0" z1="0" x2="15" y2="20" z2="60" type="stone"/>
                     
                     <!-- ceiling -->
-                    <DrawCuboid x1="0" y1="10" z1="0" x2="15" y2="10" z2="30" type="stone"/>
+                    <DrawCuboid x1="0" y1="20" z1="0" x2="15" y2="20" z2="60" type="stone"/>
+                    
+                    <!-- floor -->
+                    <DrawCuboid x1="0" y1="1" z1="0" x2="15" y2="1" z2="60" type="stone"/>
                     
                     <!-- far wall -->
-                    <DrawCuboid x1="0" y1="0" z1="30" x2="15" y2="10" z2="30" type="stone"/>
+                    <DrawCuboid x1="0" y1="0" z1="60" x2="15" y2="20" z2="60" type="stone"/>
                     
                     <!-- close wall -->
-                    <DrawCuboid x1="0" y1="0" z1="0" x2="15" y2="10" z2="0" type="stone"/>
+                    <DrawCuboid x1="0" y1="0" z1="0" x2="15" y2="20" z2="0" type="stone"/>
                     
-                    <!-- glowstones at z = 1 -->
-                    <DrawBlock x="0" y="6" z="1" type="glowstone"/>
-                    <DrawBlock x="15" y="6" z="1" type="glowstone"/>
-                    <DrawBlock x="4" y="9" z="1" type="glowstone"/>
-                    <DrawBlock x="8" y="9" z="1" type="glowstone"/>
-                    <DrawBlock x="12" y="9" z="1" type="glowstone"/>
-                    
-                    <!-- glowstones at z = 5 -->
-                    <DrawBlock x="0" y="6" z="5" type="glowstone"/>
-                    <DrawBlock x="15" y="6" z="5" type="glowstone"/>
-                    <DrawBlock x="4" y="9" z="5" type="glowstone"/>
-                    <DrawBlock x="8" y="9" z="5" type="glowstone"/>
-                    <DrawBlock x="12" y="9" z="5" type="glowstone"/>
+                    <!-- glowstones at z = 0 (close wall) -->
+                    <DrawBlock x="4" y="9" z="0" type="glowstone"/>
+                    <DrawBlock x="8" y="9" z="0" type="glowstone"/>
+                    <DrawBlock x="12" y="9" z="0" type="glowstone"/>
                     
                     <!-- glowstones at z = 10 -->
-                    <DrawBlock x="0" y="6" z="10" type="glowstone"/>
-                    <DrawBlock x="15" y="6" z="10" type="glowstone"/>
-                    <DrawBlock x="4" y="9" z="10" type="glowstone"/>
-                    <DrawBlock x="8" y="9" z="10" type="glowstone"/>
-                    <DrawBlock x="12" y="9" z="10" type="glowstone"/>
-                    
-                    <!-- glowstones at z = 15 -->
-                    <DrawBlock x="0" y="6" z="15" type="glowstone"/>
-                    <DrawBlock x="15" y="6" z="15" type="glowstone"/>
-                    <DrawBlock x="4" y="9" z="15" type="glowstone"/>
-                    <DrawBlock x="8" y="9" z="15" type="glowstone"/>
-                    <DrawBlock x="12" y="9" z="15" type="glowstone"/>
+                    <DrawBlock x="0" y="9" z="10" type="glowstone"/>
+                    <DrawBlock x="15" y="9" z="10" type="glowstone"/>
+                    <DrawBlock x="4" y="20" z="10" type="glowstone"/>
+                    <DrawBlock x="8" y="20" z="10" type="glowstone"/>
+                    <DrawBlock x="12" y="20" z="10" type="glowstone"/>
                     
                     <!-- glowstones at z = 20 -->
-                    <DrawBlock x="0" y="6" z="20" type="glowstone"/>
-                    <DrawBlock x="15" y="6" z="20" type="glowstone"/>
-                    <DrawBlock x="4" y="9" z="20" type="glowstone"/>
-                    <DrawBlock x="8" y="9" z="20" type="glowstone"/>
-                    <DrawBlock x="12" y="9" z="20" type="glowstone"/>
+                    <DrawBlock x="0" y="9" z="20" type="glowstone"/>
+                    <DrawBlock x="15" y="9" z="20" type="glowstone"/>
+                    <DrawBlock x="4" y="20" z="20" type="glowstone"/>
+                    <DrawBlock x="8" y="20" z="20" type="glowstone"/>
+                    <DrawBlock x="12" y="20" z="20" type="glowstone"/>
                     
-                    <!-- glowstones at z = 25 -->
-                    <DrawBlock x="0" y="6" z="25" type="glowstone"/>
-                    <DrawBlock x="15" y="6" z="25" type="glowstone"/>
-                    <DrawBlock x="4" y="9" z="25" type="glowstone"/>
-                    <DrawBlock x="8" y="9" z="25" type="glowstone"/>
-                    <DrawBlock x="12" y="9" z="25" type="glowstone"/>
+                    <!-- glowstones at z = 30 -->
+                    <DrawBlock x="0" y="9" z="30" type="glowstone"/>
+                    <DrawBlock x="15" y="9" z="30" type="glowstone"/>
+                    <DrawBlock x="4" y="20" z="30" type="glowstone"/>
+                    <DrawBlock x="8" y="20" z="30" type="glowstone"/>
+                    <DrawBlock x="12" y="20" z="30" type="glowstone"/>
                     
-                    <!-- glowstones at z = 29 -->
-                    <DrawBlock x="0" y="6" z="29" type="glowstone"/>
-                    <DrawBlock x="15" y="6" z="29" type="glowstone"/>
-                    <DrawBlock x="4" y="9" z="29" type="glowstone"/>
-                    <DrawBlock x="8" y="9" z="29" type="glowstone"/>
-                    <DrawBlock x="12" y="9" z="29" type="glowstone"/>
+                    <!-- glowstones at z = 40 -->
+                    <DrawBlock x="0" y="9" z="40" type="glowstone"/>
+                    <DrawBlock x="15" y="9" z="40" type="glowstone"/>
+                    <DrawBlock x="4" y="20" z="40" type="glowstone"/>
+                    <DrawBlock x="8" y="20" z="40" type="glowstone"/>
+                    <DrawBlock x="12" y="20" z="40" type="glowstone"/>
                     
+                    <!-- glowstones at z = 50 -->
+                    <DrawBlock x="0" y="9" z="50" type="glowstone"/>
+                    <DrawBlock x="15" y="9" z="50" type="glowstone"/>
+                    <DrawBlock x="4" y="20" z="50" type="glowstone"/>
+                    <DrawBlock x="8" y="20" z="50" type="glowstone"/>
+                    <DrawBlock x="12" y="20" z="50" type="glowstone"/>
+                    
+                    <!-- glowstones at z = 60 (far wall) -->
+                    <DrawBlock x="4" y="9" z="60" type="glowstone"/>
+                    <DrawBlock x="8" y="9" z="60" type="glowstone"/>
+                    <DrawBlock x="12" y="9" z="60" type="glowstone"/>
+                    
+                    <!-- ghast spawns at x="6.5" y="6" z="58" -->
+                    <DrawEntity x="6.5" y="4" z="58" type="Ghast"/>
+                    
+                    <!-- glass barricade (to prevent Ghast from moving towards agent) -->
+                    <DrawCuboid x1="0" y1="0" z1="50" x2="15" y2="4" z2="50" type="glass"/>
+                    <DrawCuboid x1="0" y1="8" z1="50" x2="14" y2="19" z2="59" type="glass"/>
                   
                   </DrawingDecorator>
                   
@@ -104,7 +109,7 @@ missionXML='''<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
               <AgentSection mode="Survival">
                 <Name>MalmoTutorialBot</Name>
                 <AgentStart>
-                  <Placement x="7" y="3" z="2" pitch="0" yaw="90"/>
+                  <Placement x="7" y="3" z="2" pitch="0" yaw="0"/>
                 </AgentStart>
                 <AgentHandlers>
                   <ObservationFromFullStats/>
@@ -159,13 +164,13 @@ print("Mission running ", end=' ')
 
 #### Moving the agent in circle so i can see what the environment looks like ####
 # Turn left 90 degrees to face down the alley
-agent_host.sendCommand("turn -1")
-time.sleep(0.5)  # 180 deg/s for 0.5s = ~90 degrees
-agent_host.sendCommand("turn 0")
+# agent_host.sendCommand("turn -1")
+# time.sleep(0.5)  # 180 deg/s for 0.5s = ~90 degrees
+# agent_host.sendCommand("turn 0")
 
 # Start alternating left/right movement
 strafe_direction = -1  # Start by strafing left
-strafe_duration = 1.5  # seconds per strafe direction
+strafe_duration = 2  # seconds per strafe direction
 last_switch_time = time.time()
 
 # Loop until mission ends:
@@ -193,5 +198,3 @@ agent_host.sendCommand("strafe 0")
 print()
 print("Mission ended")
 # Mission has ended.w
-
-
